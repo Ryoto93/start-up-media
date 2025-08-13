96bdffbf73b8d404bc6a9a971823c69bf63375e7
Ryoto93 <ryouto09030311@gmail.com>
2025-08-13 16:24:28 +0900

    最終仕上げ

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      { protocol: 'https', hostname: 'readdy.ai' },
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
-      { protocol: 'https', hostname: 'lh4.googleusercontent.com' },
-      { protocol: 'https', hostname: 'lh5.googleusercontent.com' },
-      { protocol: 'https', hostname: 'lh6.googleusercontent.com' },
-      { protocol: 'https', hostname: 'placehold.co' },
-    ],
-  },
+	images: {
+		remotePatterns: [
+			{ protocol: 'https', hostname: 'readdy.ai' },
+			{ protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+			{ protocol: 'https', hostname: 'lh4.googleusercontent.com' },
+			{ protocol: 'https', hostname: 'lh5.googleusercontent.com' },
+			{ protocol: 'https', hostname: 'lh6.googleusercontent.com' },
+			{ protocol: 'https', hostname: 'placehold.co' },
+			{ protocol: 'https', hostname: 'selnkvhmuevdnakyndup.supabase.co' },
+		],
+	},
 };
 
 export default nextConfig;
